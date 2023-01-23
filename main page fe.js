214d15ab59d7c50eb455d64f3698de92724c318d
Abdou <khobiziziabdou@gamil.com>
2023-01-23 23:45:53 +0100

    vol4

--- main page fe.js
+++ main page fe.js
@@ -6,11 +6,12 @@ function change() {
       "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAd1JREFUWEftl+FRwzAMhd/bACZgBZgARigTQDeACaATwAa0E8AGlA3YgDIBbCBOOTtnTGwpaVN6vequP9I4yucnyVKIHTPuGA/2D0hE7gHcBaVnJPV6sK2t0AFIRK5Jzksx6KOQ5UvfUQ2ZiCjIFYA5yWkXlIhcANCf2pLksrDO9FUF0t0AeEqcF6GsDE42FpdOS6p7FTIdVUKab2xBUv/rNLPKkt39cSQiRwDOAZwG7+8A3kh+p2+r+cipTCB9IE/GAPIAoLRTzZfbFMyT0GZSF5JT1XgFoOrUbAXgkqSq5jaXQtFbUObDARMfUaizPIQ1ur5AsXTdOwZQTeJBORTySEP01YckWXvsVemXQuHUlcSRVkxz0InIBMDzQCDNpZfgRw9RrczWSM7iRQ6UwuiatntnLaIvV9UPyZZj54HyWabtTRsOWex9jdLpDOWuslDy201qKzE6mqT1iN4fp+yT0teT98RDAuBT+5y35JvwOR23y0REW4eWsAWlMJNRWkehuT6G4a1rTwsAN6M019rUGBJdKyYdP7QyS+OHOeT9x4BWhSoCdYywvaoljeMYI2wRJgz5sTe1vS9PLO/UaFaZNelt9TPIcyT0AfL4MxWynByALIU2fX/tkO090A9BYx80CTXGugAAAABJRU5ErkJggg==";
     document.getElementById("main").style.background =
       "linear-gradient(145deg, #e2e1e1, #f9f7f7)";
+    document.getElementById("login").style.backgroundColor = "white";
     let me = document.querySelectorAll("#first,#second,#third,#forth");
     for (let i = 0; i < me.length; i++) {
       me[i].style.color = "black";
     }
-
+    /* this is for page responsivity*/
     if (screen.width <= "768") {
       document.getElementById("phonelist").style.backgroundColor =
         "transparent";
@@ -25,10 +26,12 @@ function change() {
       "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAnpJREFUWEftV9FRGzEU3K2AdABUkFAB0AFUkFBBoIJABUAHUAFQQaACSAekA1LBMpuRPLpn3Vnnwx6Y4c34w+eztNq3b98T8c6C7wwPPj4gSVsAvgP4kdjdJ/n8Vkw3MyTpG4BfAA7C5mckT9cKSJLZOAfwpbLxIcnbtQGSdAHg58CGTtn9WgBJukp6yfs9ADgG8JgfkGxOewvo3sUkeWOnKcc1yf9ClqS1AkoCnrEAYAYmAXJVbSZQ2yuvMknWxG7a8A9JV9gswu+r1ZCkPQC/i/3nGJDkMrcFOFZb9kHInVRlkJLsRTfp+zPJ7RbBtrwzJ2pJLwA20p93SD7FhSTZj6yj/N4RSVfk5OgACmL+S9JtohohbQZn8D7MpIiASv08kPT3PkCRpXuS+5PQAN1uP1asQUvGckXyaAqoyNDo6gmHMBZbhvvbUumbDMgIKi3GYI5JXo9lKwIqy/mOZBw1etevMOV3LXYz5mnghaR7oQ/gYvkKwIZra5nNU0NVNtpfkqZc/tkOWgjq7LOUDw3tkjzKjdmfFmAde6kBKkeOqlO3HDulxim3dTg1tgmnKUan9dQALexlrYBq71UmiU6vrM5DoZu/ieFlcGHtuQz0AYosXZK0JiZFsId/TmfslUMTY2mSBnJB8mRZRBWvql4OBufhyiL2k5MxE2LyHI8q5ZDXy/giQK4M3zp8McxhF/azjqFF5pJ4/b+Y6sHKbbox9LhwdmLPS+XMZCb8qY0uZteH6Y0mQMlTLHTrKs/aY+TklnHacn9rBlSUra9C2fCGnNhVZM15JGm+SI4GVNKSLgTWWSlYp8+NtBlEueYkQGNy1vruJ6BFTL0CeqgVNEJIAWcAAAAASUVORK5CYII=";
     document.getElementById("main").style.background =
       "linear-gradient(145deg, #121212, #121212)";
+    document.getElementById("login").style.backgroundColor = "gray";
     let me = document.querySelectorAll("#first,#second,#third,#forth");
     for (let i = 0; i < me.length; i++) {
       me[i].style.color = "white";
     }
+    /* this is for page responsivity*/
     if (screen.width <= "768") {
       document.getElementById("phonelist").style.backgroundColor = "#121212";
     } else {
@@ -38,3 +41,12 @@ function change() {
   }
 }
 
+function logindisplay() {
+  let log = document.getElementById("login");
+  if (log.style.display=="block") {
+    log.style.display = "none";
+  } else {
+    log.style.display = "block";
+    console.log("efe");
+  }
+}
